23b999f982d6d8cf1025da8d890a12956e0c394a
Adeniyi <teebarg01@gmail.com>
2023-10-27 11:43:20 +0100

    first commit

---
+++ src/app/app-download/page.jsx
@@ -0,0 +1,12 @@
+import { Header } from "@/components/Header";
+
+export default function AppDownload() {
+    return (
+        <>
+            <Header />
+            <div className="px-2">
+                <div className="mx-auto max-w-3xl">Download App Here</div>
+            </div>
+        </>
+    );
+}
