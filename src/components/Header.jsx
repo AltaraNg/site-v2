aa25b63ec69feac28d3f1e8368e7302cbaa160e3
Damola Oladeji <darmhoo@hotmail.com>
2024-02-21 06:43:53 +0100

    fixbugs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -62,7 +62,7 @@ function MobileNavigation() {
                         className="absolute inset-x-0 top-full mt-4 flex origin-top flex-col rounded-2xl bg-white p-4 text-lg tracking-tight text-slate-900 shadow-xl ring-1 ring-slate-900/5"
                     >
                         <MobileNavLink href="#features">Features</MobileNavLink>
-                        <MobileNavLink href="/#collections">Collections</MobileNavLink>
+                        <MobileNavLink href="/collections">Collections</MobileNavLink>
 
                         <MobileNavLink href="#testimonials">Testimonials</MobileNavLink>
                         <MobileNavLink href="#offers">Offers</MobileNavLink>
